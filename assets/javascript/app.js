2b3f111fac0676053d2ed1c0d9f5b96e5f17c9d0
Corey Richard <ullrtheglorious@gmail.com>
2018-03-10 11:15:16 -0700

    restructure HTML links

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,12 @@
-
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBduDTXB2PTUnzWhxbVATEwjbyLYMjgPf0",
+    authDomain: "cdc-project-1.firebaseapp.com",
+    databaseURL: "https://cdc-project-1.firebaseio.com",
+    projectId: "cdc-project-1",
+    storageBucket: "cdc-project-1.appspot.com",
+    messagingSenderId: "701148576356"
+};
+firebase.initializeApp(config);
 
 
