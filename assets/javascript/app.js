88a69830f04d9d3492c785921712c6b8ea25084c
Corey Richard <ullrtheglorious@gmail.com>
2018-03-10 11:50:15 -0700

    carousel changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
 // Initialize Firebase
-var config = {
-    apiKey: "AIzaSyBduDTXB2PTUnzWhxbVATEwjbyLYMjgPf0",
-    authDomain: "cdc-project-1.firebaseapp.com",
-    databaseURL: "https://cdc-project-1.firebaseio.com",
-    projectId: "cdc-project-1",
-    storageBucket: "cdc-project-1.appspot.com",
-    messagingSenderId: "701148576356"
-};
-firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyBduDTXB2PTUnzWhxbVATEwjbyLYMjgPf0",
+//     authDomain: "cdc-project-1.firebaseapp.com",
+//     databaseURL: "https://cdc-project-1.firebaseio.com",
+//     projectId: "cdc-project-1",
+//     storageBucket: "cdc-project-1.appspot.com",
+//     messagingSenderId: "701148576356"
+// };
+// firebase.initializeApp(config);
 
-$('.carousel').carousel({
-    interval: 2000
+$('#carousel').carousel({
+    interval: 6000
 })
 
 
