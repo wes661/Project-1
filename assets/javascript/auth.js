740479f6ddf3d6f4b77d76044027f3c0b5675740
Patrick Marrin <pmarrin13@gmail.com>
2018-03-13 19:59:42 -0700

    adding spoonacular ajax call

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,36 +1,12 @@
-// const auth = firebase.auth();
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBduDTXB2PTUnzWhxbVATEwjbyLYMjgPf0",
+    authDomain: "cdc-project-1.firebaseapp.com",
+    databaseURL: "https://cdc-project-1.firebaseio.com",
+    projectId: "cdc-project-1",
+    storageBucket: "cdc-project-1.appspot.com",
+    messagingSenderId: "701148576356"
+  };
+  firebase.initializeApp(config);
 
-// // get html elemants
-// const txtEmail = document.querySelector("#txtEmail");
-// const txtPassword = document.querySelector("#txtPassword");
-// const btnLogin = document.querySelector("#btnLogin");
-// const btnSignUp = document.querySelector("#btnSignUp");
-// const btnLogout = document.querySelector("#btnLogout");
-// const btnAnony = document.querySelector("#btnAnony")
-
-// let provider = new firebase.auth.GoogleAuthProvider();
-
-//   firebase.auth().signInWithPopup(provider).then(function(result) {
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     var token = result.credential.accessToken;
-//     // The signed-in user info.
-//     var user = result.user;
-//     // ...
-//   }).catch(function(error) {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // The email of the user's account used.
-//     var email = error.email;
-//     // The firebase.auth.AuthCredential type that was used.
-//     var credential = error.credential;
-//     // ...
-//   });
-//     firebase.auth().signInWithPopup(provider);
   
-
-// //  btnLogin.addEventListener('click', e => {
-// //      e.preventDefault();
-// //      auth.signInWithPopup(provider);
-// //      console.log("working");
-// //  })
--- assets/javascript/spoonApi.js
+++ assets/javascript/spoonApi.js
@@ -1,6 +1,48 @@
 // key
 // c4YgIGcU8WmshZ3ig1VyeaajeldQp1cNxb7jsnu5l0L2zf7GbS
 
+// apples%2Cflour%2Csugar //way to typw multiple ingredients
+
 // Url
 //spoonacular-recipe-food-nutrition-v1.p.mashape.com
+        
+        
+$("#button").on("click", function(e) {
+    e.preventDefault();
 
+    let ingredient1 = ($("#ingredient1").val().trim());
+    let ingredient2 = ("+" + $("#ingredient2").val().trim());
+    let ingredient3 = ("+" + $("#ingredient3").val().trim());
+    let ingredient4 = ("+" + $("#ingredient4").val().trim());
+    let ingredient5 = ("+" + $("#ingredient5").val().trim());
+    let ingredient6 = ("+" + $("#ingredient6").val().trim());
+    let ingredients = (ingredient1 + ingredient2 + ingredient3 + ingredient4 + ingredient5 + ingredient6);
+
+    //recipe search url
+    let queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?number=1&offset=0&query=" + ingredients;
+
+    //recipe for title
+    // let queryUrl = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=http%3A%2F%2Fwww.melskitchencafe.com%2Fthe-best-fudgy-brownies%2F";
+
+    console.log(ingredients);
+
+    $.ajax({
+      url: queryUrl,
+      method: "GET",
+      beforeSend: function(xhr){
+        xhr.setRequestHeader('X-Mashape-Key', 'c4YgIGcU8WmshZ3ig1VyeaajeldQp1cNxb7jsnu5l0L2zf7GbS');
+        }
+  }).then(function(response) {
+
+    console.log(response);
+    console.log(response.results[0].image);
+    
+    let picSrc = response.results[0].image;
+
+    let image = $("<image>");
+    image.attr("src", picSrc);
+    $("#picDiv").append(image);
+
+  });
+
+});
