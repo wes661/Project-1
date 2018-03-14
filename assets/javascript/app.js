93d097408175fd8a1eea66ebc86400aed7bedc40
JoeyRyanBridges <joeyryanbridges@gmail.com>
2018-03-13 21:43:29 -0700

    resolving conflicts

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,4 +46,89 @@ $("#addMore").on("click", function () {
     }
 })
 
+// Initialize Map //
+var map;
+var infoWindow;
+function generateMap(kw, rad){
+    console.log('running');
+    infoWindow = new google.maps.InfoWindow;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+        var pyrmont = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
 
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Your Location.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+
+        var request = {
+            location: pyrmont,
+            radius: rad,
+            keyword: kw
+        };
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+        }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 32.2217, lng: -110.9265},
+        zoom: 10
+        });
+}
+    
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                            'Error: The Geolocation service failed.' :
+                            'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}    
+    
+function callback(results, status) {
+    console.log("Running");
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        createMarkers(results);
+    }
+}
+
+function createMarkers(places) {
+    var bounds = new google.maps.LatLngBounds();
+
+    for (var i = 0, place; place = places[i]; i++) {
+    
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    var marker = new google.maps.Marker({
+        map: map,
+        title: place.name,
+        position: place.geometry.location 
+    });
+
+    bounds.extend(place.geometry.location);
+    }
+    map.fitBounds(bounds);
+}
+
+
+
+//Get user location and radius of stores    
+$(document).ready(function(){
+    console.log('running');
+    $('#getStores').on('click', function(){
+        var rad = 4828.03;
+        if($('#radiusInput').val() != 0){
+            rad = $('#radiusInput').val()*1609.34;
+        }
+        generateMap(['grocery', 'store'], rad);
+    })
+})
