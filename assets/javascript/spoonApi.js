f5356315cb11919a963ca2be4f29b65fbc399c25
Patrick Marrin <pmarrin13@gmail.com>
2018-03-14 19:00:09 -0700

    images now push to second page

--- assets/javascript/spoonApi.js
+++ assets/javascript/spoonApi.js
@@ -14,7 +14,7 @@ $("#recipeSearch").on("click", function(e) {
     console.log(search);
 
 
-    let queryUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=3&tags=' + search;
+    let queryUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=9&tags=' + search;
 
     $.ajax({
       url: queryUrl,
@@ -28,9 +28,6 @@ $("#recipeSearch").on("click", function(e) {
     let number = 1
 
     for ( let i = 0; i < response.recipes.length; i++) {
-      console.log(recipes[i]);
-      console.log(recipes[i].image);
-      console.log(recipes[i].title);
 
       let recipeTitle = recipes[i].title
       let recipeImage = recipes[i].image
@@ -47,31 +44,42 @@ $("#recipeSearch").on("click", function(e) {
   });
 });
 
-database.ref("recipeCards").on('child_added', snapshot => {
+database.ref("recipeCards").on('value', function(snapshot) {
+
+  console.log(snapshot.val().recipe1.title);
+
+  let recipe1Picture = snapshot.val().recipe1.picture;
+  let recipe2Picture = snapshot.val().recipe2.picture;
+  let recipe3Picture = snapshot.val().recipe3.picture;
+  let recipe4Picture = snapshot.val().recipe4.picture;
+  let recipe5Picture = snapshot.val().recipe5.picture;
+  let recipe6Picture = snapshot.val().recipe6.picture;
+  let recipe7Picture = snapshot.val().recipe7.picture;
+  let recipe8Picture = snapshot.val().recipe8.picture;
+  let recipe9Picture = snapshot.val().recipe9.picture;
+
   
-  for(let i = 0; i < snapshot.length; i++) {
-    console.log(snapshot[i].title);
-  }
+  $("#basicModal-1").html(snapshot.val().recipe1.title);
+  $("#basicModal-2").html(snapshot.val().recipe2.title);
+  $("#basicModal-3").html(snapshot.val().recipe3.title);
+  $("#basicModal-4").html(snapshot.val().recipe4.title);
+  $("#basicModal-5").html(snapshot.val().recipe5.title);
+  $("#basicModal-6").html(snapshot.val().recipe6.title);
+  $("#basicModal-7").html(snapshot.val().recipe7.title);
+  $("#basicModal-8").html(snapshot.val().recipe8.title);
+  $("#basicModal-9").html(snapshot.val().recipe9.title);
 
-})
+  $("#recipePic1").attr("src", recipe1Picture);
+  $("#recipePic2").attr("src", recipe2Picture);
+  $("#recipePic3").attr("src", recipe3Picture);
+  $("#recipePic4").attr("src", recipe4Picture);
+  $("#recipePic5").attr("src", recipe5Picture);
+  $("#recipePic6").attr("src", recipe6Picture);
+  $("#recipePic7").attr("src", recipe7Picture);
+  $("#recipePic8").attr("src", recipe8Picture);
+  $("#recipePic9").attr("src", recipe9Picture);
 
 
+  
+});
 
-
-
-
-
-      // let cardDiv = $("<div>");
-      // cardDiv.addClass(" text-center  col-md-3");
-      // $("#test").append(cardDiv);
-
-      // let cardTitle = $("<h3>");
-      // cardTitle.addClass("card-header");
-      // cardTitle.text(recipeTitle);
-      // cardDiv.append(cardTitle);
-
-
-      // let cardImg= $("<img>");
-      // cardImg.addClass("img-fluid card-body");
-      // cardImg.attr("src", recipes[i].image);
-      // cardDiv.append(cardImg);
