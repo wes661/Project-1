98d17ca2562b2f6b72448050dee090e9d3dd16c6
Wesley Hanson <wrh1600@gmail.com>
2018-03-14 22:23:36 -0700

    fixed mapBtn,can scroll on modal,changed recipe-btn,setTimeout to modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,6 +51,21 @@ function secondPageLoad() {
     }, 3000)
 };
 
+
+
+
+
+$(".recipe-btn").on("click", function(){
+    setTimeout(function(){
+        $('div.modal, div.fade, div.bd-example-modal-lg').modal('show');
+    }, 3000);
+
+})
+
+    
+ 
+
+
 function loading() {
     $("#spin").show();
     $("#spin").addClass("fa-spin");
@@ -142,10 +157,11 @@ function createMarkers(places) {
 
 //Get user location and radius of stores    
 $(document).ready(function(){
-    $('#getStores').on('click', function(){
+    $('#mapBtn').on('click', function(){
+        $("#map").slideDown(2000);
         var rad = 4828.03;
-        if($('#radiusInput').val() != 0){
-            rad = $('#radiusInput').val()*1609.34;
+        if($('#miles').val() != 0){
+            rad = $('#miles').val()*1609.34;
         }
         generateMap(['grocery', 'store'], rad);
     })
