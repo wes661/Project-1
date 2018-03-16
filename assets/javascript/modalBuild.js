851c2707e8073ee60b53229e04252c5ca65fc37c
Patrick Marrin <pmarrin13@gmail.com>
2018-03-16 00:14:03 -0700

    set up spot for modal in html and layout on modal fixed

--- assets/javascript/modalBuild.js
+++ assets/javascript/modalBuild.js
@@ -3,46 +3,65 @@ $(".recipe-btn").on("click", function () {
     var j = idArr[idArr.length - 1];
     //console.log(idArr);
     database.ref('recipeCards/recipe' + j).once('value', function (data) {
+        $("#list").empty();
 
-        let modalCard = $("<div>");
-        modalCard.addClass("card text-center");
+        $("#title").html(data.val().title);
+        $("#likes").text(data.val().likes);
+        $("#time").text(data.val().time);
+        $("#diets").text(data.val().diets);
+        $("#image").attr("src", data.val().picture);
+        $("#instructions").text(data.val().instructions);
+        $("#url").html(data.val().url);
 
-        let modalHeader = $("<div>");
-        modalHeader.addClass("card-header");
-        modalHeader.html("<h2>" + data.val().title);
-        modalCard.append(modalHeader);
-
-        let row = $('<div>');
-        row.addClass("row");
-
-
-        let modalImage = $("<img>");
-        modalImage.addClass("img-fluid col-12");
-        modalImage.attr("src", data.val().picture);
-        row.append(modalImage);
 
         for (let i = 0; i < data.val().ingredients.length; i++) {
-            let div = $("<div>");
             let ul = $("<ul>");
             let li = $("<li>");
             li.text(data.val().ingredients[i]);
             ul.append(li);
-            div.append(ul);
-            row.append(div);
+            $("#list").append(ul);
         }
 
-        modalCard.append(row);
 
-        let modalBody = $("<div>");
-        modalBody.addClass("card-body");
-        modalCard.append(modalBody);
+        // let modalCard = $("<div>");
+        // modalCard.addClass("card text-center");
 
-        let modalInstructions = $("<p>");
-        modalInstructions.text(data.val().instructions);
-        modalBody.append(modalInstructions);
+        // let modalHeader = $("<div>");
+        // modalHeader.addClass("card-header");
+        // modalHeader.html("<h2>" + data.val().title);
+        // modalCard.append(modalHeader);
 
 
-        $("#modal-body").html(modalCard);
+        // // let likes = $("<div>");
+        // // let ie = $("<i>");
+        // // ie.html("<i>" + "Likes  " + data.val().likes + "</i>");
+        // // console.log(likes);
+        // // ie.appendTo(likes);
+        // // modalHeader.append(likes);
+
+
+        // let row = $('<div>');
+        // row.addClass("row");
+
+        // let modalImage = $("<img>");
+        // modalImage.addClass("img-fluid col-12");
+        // modalImage.attr("src", data.val().picture);
+        // row.append(modalImage);
+
+
+
+        // modalCard.append(row);
+
+        // let modalBody = $("<div>");
+        // modalBody.addClass("card-body");
+        // modalCard.append(modalBody);
+
+        // let modalInstructions = $("<p>");
+        // modalInstructions.text(data.val().instructions);
+        // modalBody.append(modalInstructions);
+
+
+        // $("#modal-body").html(modalCard);
 
 
     });
